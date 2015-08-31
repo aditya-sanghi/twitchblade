3bd80ae07725e6eef7f7fe1072cc4bd423ed4a8c
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-08-31 11:28:47 +0530

    spec for login in cli-had missed it before adding login to cli

--- spec/twitchblade/cli_spec.rb
+++ spec/twitchblade/cli_spec.rb
@@ -16,12 +16,22 @@ module Twitchblade
 
       end
 
-      it 'should create a new user' do
+      it 'should call signup feature for the user' do
         cli = Cli.new(@connection)
         user = User.new("aditya", "pass123", @connection)
         allow(Kernel).to receive(:gets).and_return(1, "aditya", "pass123",)
         allow(User).to receive(:new).and_return(user)
         expect(user).to receive(:signup)
+        cli.take_input_and_call_feature
+      end
+
+
+      it 'should call login feature for the user' do
+        cli = Cli.new(@connection)
+        user = User.new("aditya", "pass123", @connection)
+        allow(Kernel).to receive(:gets).and_return(2, "aditya", "pass123",)
+        allow(User).to receive(:new).and_return(user)
+        expect(user).to receive(:login)
         cli.take_input_and_call_feature
       end
       
