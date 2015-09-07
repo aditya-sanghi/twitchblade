cef42c14331ac0b44b94a605b38dcd702e429f7c
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-09-07 12:57:23 +0530

    added spec for signup inside dispatcher specs

--- spec/twitchblade/dispatcher_spec.rb
+++ spec/twitchblade/dispatcher_spec.rb
@@ -13,7 +13,15 @@ module Twitchblade
         allow(Kernel).to receive(:gets).and_return("aditya", "pass123")
         expect(User).to receive(:new).and_return(User.new("aditya", "pass123", @connection))
         dispatcher.invoke_feature
+      end
 
+      it 'should call signup feature for the user' do
+        dispatcher = Dispatcher.new(@connection, 1)
+        user = User.new("aditya", "pass123", @connection)
+        allow(User).to receive(:new).and_return(user)
+        allow(Kernel).to receive(:gets).and_return("aditya", "pass123")
+        expect(user).to receive(:signup)
+        dispatcher.invoke_feature
       end
     end
   end
