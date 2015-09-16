925ecbb350c0bb6591601b0aaca66f2f4053bcd4
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-09-16 18:36:42 +0530

    spec for retweet

--- lib/twitchblade/tweet.rb
+++ lib/twitchblade/tweet.rb
@@ -12,14 +12,37 @@ module Twitchblade
 
     def make_tweet
       @user_id = @connection.exec("select user_id from user_info where user_name = '#{@user_name}'").field_values('user_id')[0].to_i
-      insertion = @connection.exec_params("INSERT INTO tweets (user_id, tweet_content) VALUES ($1, $2)", [@user_id, input])
+      inserted_tweet = @connection.exec_params("INSERT INTO tweets (user_id, tweet_content) VALUES ($1, $2)", [@user_id, input])
       current_tweet_id = @connection.exec("select tweet_id from tweets where user_id = '#{@user_id.to_i}' order by lastmodified desc limit 1").field_values('tweet_id')[0]
-      insertion.cmd_tuples == 1 ? current_tweet_id : false
+      inserted_tweet.cmd_tuples == 1 ? current_tweet_id : false
     end
 
     def display_tweet
       tweet = @connection.exec("select tweet_content from tweets where user_id = '#{@user_id.to_i}' order by lastmodified desc limit 1").field_values('tweet_content')[0].to_s
       puts tweet
+    end
+
+    def retweet
+      tweet_id_to_be_retweeted = input
+      find_tweet = @connection.exec("select tweet_id from tweets where tweet_id = #{tweet_id_to_be_retweeted}")
+      if find_tweet.ntuples == 0
+        puts "There is no tweet with this tweet ID to retweet!"
+        false
+      else
+        retweet_content = @connection.exec("SELECT TWEET_CONTENT from tweets where tweet_id = #{tweet_id_to_be_retweeted}").field_values('tweet_content')[0]
+        new_tweet_id = @connection.exec_params("INSERT into tweets (user_id, tweet_content) VALUES ($1, $2) returning TWEET_ID", [@user_id, retweet_content]).field_values('TWEET_ID')[0]
+        origin_tweet_id = @connection.exec("select tweet_id from RETWEETS where tweet_id = #{tweet_id_to_be_retweeted}").field_values('TWEET_ID')[0]
+        if origin_tweet_id == nil
+          origin_tweet_id = tweet_id_to_be_retweeted
+        end
+        puts @connection.exec_params("INSERT into retweets (tweet_id, tweet_id_retweeted_from, origin_tweet_id ) VALUES ($1, $2, $3)", [new_tweet_id, tweet_id_to_be_retweeted, origin_tweet_id])
+      end
+
+      def display_retweet
+        tweet = @connection.exec("select tweet_content from tweets where user_id = #{@user_id.to_i} order by lastmodified desc limit 1").field_values('tweet_content')[0].to_s
+        puts "{#@user_name} retweeted "
+        puts tweet
+      end
     end
   end
 end
--- spec/twitchblade/tweet_spec.rb
+++ spec/twitchblade/tweet_spec.rb
@@ -7,20 +7,43 @@ module Twitchblade
       @connection = PG::Connection.open(:dbname => 'testing')
     end
 
-    it 'should enter the tweet in the tweet table' do
-      user_1 = User.new("aditya.sng93", "123", @connection).signup
-      tweet = Tweet.new(@connection, "aditya.sng93")
-      allow(Kernel).to receive(:gets).and_return("my tweet is being entered")
-      expect(tweet.make_tweet).to_not eq(false)
+    before(:each) do
+      @connection.exec("ALTER SEQUENCE tweets_tweet_id_seq RESTART WITH 1")
     end
 
-    it 'should have last tweet of the user as the currently entered tweet' do
-      user_1 = User.new("aditya.sng93", "123", @connection).signup
-      tweet = Tweet.new(@connection, "aditya.sng93")
-      allow(Kernel).to receive(:gets).and_return("my tweet is being entered now")
-      tweet.make_tweet
-      expect(STDOUT).to receive(:puts).with('my tweet is being entered now')
-      tweet.display_tweet
+    after(:each) do
+      @connection.exec("delete from tweets")
+      @connection.exec("delete from retweets")
+    end
+
+    context 'fresh tweet' do
+      it 'should enter the tweet in the tweet table' do
+        User.new("aditya.sng93", "123", @connection).signup
+        tweet = Tweet.new(@connection, "aditya.sng93")
+        allow(Kernel).to receive(:gets).and_return("my tweet is being entered")
+        expect(tweet.make_tweet).to_not eq(false)
+      end
+
+      it 'should have last tweet of the user as the currently entered tweet' do
+        User.new("aditya.sng93", "123", @connection).signup
+        tweet = Tweet.new(@connection, "aditya.sng93")
+        allow(Kernel).to receive(:gets).and_return("my tweet is being entered now")
+        tweet.make_tweet
+        expect(STDOUT).to receive(:puts).with('my tweet is being entered now')
+        tweet.display_tweet
+      end
+    end
+
+    context 'retweet' do
+      it 'should enter the retweeted tweet in the tweet table without asking user to input the tweet' do
+        User.new("aditya.sng931", "123", @connection).signup
+        tweet = Tweet.new(@connection, "aditya.sng931")
+        allow(Tweet).to receive(:new).and_return(tweet)
+        allow(Kernel).to receive(:gets).and_return("my tweet is being entered now")
+        tweet.make_tweet
+        allow(Kernel).to receive(:gets).and_return("1")
+        expect(tweet.retweet).to_not eq(false)
+      end
     end
   end
 end
