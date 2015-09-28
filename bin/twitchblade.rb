c35bf7000778e1bf1230191afc6d848152a590ce
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-09-28 11:01:02 +0530

    added staging as default connection in case no environment is specified

--- bin/twitchblade.rb
+++ bin/twitchblade.rb
@@ -4,14 +4,16 @@ $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..", "lib
 require 'pg'
 require 'figaro'
 begin
-  ARGV[0] = "staging"
+  if ARGV[0] == nil
+    ARGV[0] = "staging"
+  end
   Figaro.application = Figaro::Application.new(environment: ARGV[0], path: "../config/application.yml")
   Figaro.load
 
   require 'twitchblade'
   Twitchblade::Application.new.run
 
- rescue PG::ConnectionBad
- puts "Postgres Database Connection Unsuccessful!"
- puts "Kindly check the database configuration"
+rescue PG::ConnectionBad
+  puts "Postgres Database Connection Unsuccessful!"
+  puts "Kindly check the database configuration"
 end
